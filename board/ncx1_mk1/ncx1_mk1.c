48410d3306638d27f3278e903295f266ab579be3
jman <b.devcabsy@gmail.com>
2021-06-10 13:02:14 +0400

    Pull Down PC 9,10,14

--- board/ncx1_mk1/ncx1_mk1.c
+++ board/ncx1_mk1/ncx1_mk1.c
@@ -152,8 +152,10 @@ static void ddramc_init(void)
 
 static void at91_green_led_on(void)
 {
-	// pio_set_gpio_output(AT91C_PIN_PC(0), 0); // jolp add
-	// pio_set_gpio_output(AT91C_PIN_PC(1), 0); // jolp add
+	pio_set_gpio_output(AT91C_PIN_PC(9), 0); // jolp add sensor
+	pio_set_gpio_output(AT91C_PIN_PC(10), 0); // jolp add sensor
+	pio_set_gpio_output(AT91C_PIN_PC(14), 0); // jolp add sensor
+
 	pio_set_gpio_output(AT91C_PIN_PB(14), 0); // jolp add
 	pio_set_gpio_output(AT91C_PIN_PB(13), 0);
 	pio_set_gpio_output(AT91C_PIN_PB(11), 0);
